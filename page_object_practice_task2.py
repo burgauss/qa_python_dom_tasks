5da911d793861fedf6072271ccfa4f1961bf5f6f
Juan Burgos <burgos.135@gmail.com>
2025-05-04 21:12:39 -0600

    finish page object practice task 2

--- page_object_practice_task2.py
+++ page_object_practice_task2.py
@@ -1,3 +1,5 @@
+import time
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support import expected_conditions
@@ -31,7 +33,7 @@ class LoginPageAround:
 # Clase para el encabezado
 class HeaderPageAround:
     # Crea un localizador para el elemento Correo electrónico en el encabezado
-    header_user = ...
+    header_user = (By.CLASS_NAME, 'header__user')
 
     def __init__(self, driver):
         self.driver = driver
@@ -42,7 +44,7 @@ class HeaderPageAround:
 
     # Método para recuperar el texto del elemento en el encabezado
     def email_in_header(self):
-        ...
+        return self.driver.find_element(*self.header_user).text
 
 
 # Clase con la prueba automatizada
@@ -58,26 +60,28 @@ class TestAround:
     def test_check_email_in_header(self):
         # Abre la página de la aplicación de prueba
         self.driver.get('https://around-v1.nm.tripleten-services.com/signin?lng=es')
+        time.sleep(3)
 
         # Crea una clase de objeto de página para la página de inicio de sesión
-        ...
+        login_page = LoginPageAround(self.driver)
+
         # iniciar sesión
-        correo electrónico = "Introduce tu correo electrónico"
-        contraseña = "Introduce tu contraseña"
+        correo_electronico = 'burgos.137@gmail.com'
+        password = '12345'
         # Pasa estas variables al método.
-        ...
+        login_page.login(correo_electronico, password)
 
         # Crea un objeto para el encabezado
-        ...
+        header_page = HeaderPageAround(self.driver)
         # Espera a que se cargue el encabezado
-        ...
+        header_page.wait_for_load_header()
         # Recupera el texto de un elemento en el encabezado
-        email_from_header = ...
+        email_from_header = header_page.email_in_header()
 
         # Comprueba que el valor recuperado coincida con el correo electrónico
-        assert ...
+        assert email_from_header == correo_electronico, 'Correo electronico en header no es igual al correo ingresado'
 
     @classmethod
     def teardown_class(cls):
         # Cerrar el navegador
-        ...
+        cls.driver.quit()
